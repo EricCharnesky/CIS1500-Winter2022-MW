58d0f072a0a24e606da5b31b77bf33356bcaee33
Eric Charnesky <eric.charnesky@careevolution.com>
2022-02-09 10:57:19 -0500

    project 2 methods

---
+++ Project2/src/project2/Project2.java
@@ -0,0 +1,16 @@
+
+package project2;
+
+
+public class Project2 {
+
+    public static void main(String[] args) {
+        int totalIncome = askUserToEnterIncomes();
+    }
+    
+    public static int askUserToEnterIncomes(){
+        // TODO
+        return 0;
+    }
+    
+}
