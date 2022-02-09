ac6840b5cb9b037a949c5d9be817b80171cd4bf0
Eric Charnesky <eric.charnesky@careevolution.com>
2022-02-09 11:07:43 -0500

    forgot to save project 2

--- Project2/src/project2/Project2.java
+++ Project2/src/project2/Project2.java
@@ -6,9 +6,35 @@ public class Project2 {
 
     public static void main(String[] args) {
         int totalIncome = askUserToEnterIncomes();
+        int totalDeductions = askUserToEnterTotalDeductions();
+        totalDeductions = pickLargerOfDeductionsOrStandardDeduction(totalDeductions);
+        
+        int adjustedGrossIncome = totalIncome - totalDeductions;
+        
+        int taxesOwedAt10Percent = calculatedTaxesForBracket(.1, 0, 9950, adjustedGrossIncome);
+        //...//
+        
+        // TODO
+    }
+    
+    public static int calculatedTaxesForBracket(double taxRate, 
+            int startOfBracket, int endOfBracket, int incomeToTax){
+        // TODO
+        // either none, all, or some
+        return 0;
     }
     
     public static int askUserToEnterIncomes(){
+        // TODO
+        return 0;
+    }
+    
+    public static int askUserToEnterTotalDeductions(){
+        // TODO
+        return 0;
+    }
+    
+    public static int pickLargerOfDeductionsOrStandardDeduction(int totalDeductions){
         // TODO
         return 0;
     }
